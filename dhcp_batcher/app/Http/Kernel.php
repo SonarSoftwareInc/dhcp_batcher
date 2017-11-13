0165b47c9636163cda9dfdd3287d2858b8df1199
Simon <simon@sonar.software>
2017-11-13 14:28:26 -0600

    Enforced JSON responses on the API

--- dhcp_batcher/app/Http/Kernel.php
+++ dhcp_batcher/app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\WeWantJson;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            WeWantJson::class,
+            //'throttle:60,1',
             'bindings',
         ],
     ];
