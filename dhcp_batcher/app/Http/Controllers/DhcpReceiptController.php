44609617d83a47d2c2b5c420366ba5ed70fc920b
Simon <simon@sonar.software>
2017-11-13 14:04:43 -0600

    Started work on the DHCP receiving controller

---
+++ dhcp_batcher/app/Http/Controllers/DhcpReceiptController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DhcpReceiptRequest;
+use Illuminate\Http\Request;
+
+class DhcpReceiptController extends Controller
+{
+    /**
+     * @param DhcpReceiptRequest $request
+     */
+    public function receiveDhcpAssignment(DhcpReceiptRequest $request)
+    {
+
+    }
+}
---
+++ dhcp_batcher/app/Http/Requests/DhcpReceiptRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Exceptions\FormattingException;
+use App\Rules\MacAddress;
+use Illuminate\Foundation\Http\FormRequest;
+
+class DhcpReceiptRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'leased_mac_address' => ['required', 'string', new MacAddress()],
+            'ip_address' => 'required|ip',
+            'remote_id' => ['string', new MacAddress()],
+        ];
+    }
+}
