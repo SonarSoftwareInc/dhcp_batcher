7722f341d33ad240ec91b14cdbd1613ee81cf671
Simon <simon@sonar.software>
2017-11-16 10:43:09 -0600

    Not storing microsecond precision anymore

--- dhcp_batcher/database/migrations/2017_11_13_200120_create_pending_dhcp_assignments_table.php
+++ dhcp_batcher/database/migrations/2017_11_13_200120_create_pending_dhcp_assignments_table.php
@@ -19,7 +19,7 @@ class CreatePendingDhcpAssignmentsTable extends Migration
             $table->ipAddress('ip_address');
             $table->macAddress('remote_id')->nullable();
             $table->boolean('expired');
-            $table->timestamps(6);
+            $table->timestamps();
             $table->index(['created_at', 'updated_at']);
         });
     }
